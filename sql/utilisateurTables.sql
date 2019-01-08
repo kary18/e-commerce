afe20173137261936318cc247cfd5a1873846e8f
khoucha <mook9005@gmail.com>
2019-01-08 15:39:57 +0100

    les clients

---
+++ sql/utilisateurTables.sql
@@ -0,0 +1,4 @@
+INSERT INTO clients(Id, pseudo, passe, email) VALUES (1,"Manel","toto1","manel@gmail.com");
+INSERT INTO clients(Id, pseudo, passe, email) VALUES (2, "Karima","toyota18","karima@gmail.com");
+INSERT INTO clients(Id, pseudo, passe,email) VALUES (3,"Alpha","polo12","alpha@gmail.com");
+INSERT INTO clients(Id, pseudo, passe,email) VALUES (4,"Martin","volkswagen","martin@gmail.com");
