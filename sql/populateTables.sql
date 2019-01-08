e96172b6e85fa461c29a61e0b78dcb28703bcb58
khoucha <mook9005@gmail.com>
2019-01-08 14:29:25 +0100

    Init populate script

---
+++ sql/populateTables.sql
@@ -0,0 +1,4 @@
+INSERT INTO categories(id, label) VALUES (1, 'Sacs');
+INSERT INTO categories(id, label) VALUES (2, 'Manteaux');
+INSERT INTO categories(id, label) VALUES (3, 'Robes');
+INSERT INTO categories(id, label) VALUES (4, 'Accessoires');
